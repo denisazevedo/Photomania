1883fd8a5af2c7634b83a7288576affbbbe5bc82
Denis C de Azevedo <denis.azevedo@gmail.com>
2014-03-02 15:10:11 -0300

    create photographer table view controller using CoreDataTableViewController helper as superclass

---
+++ Photomania/PhotographersCDTVC.h
@@ -0,0 +1,15 @@
+//
+//  PhotographersCDTVC.h
+//  Photomania
+//
+//  Created by Denis C de Azevedo on 02/03/14.
+//  Copyright (c) 2014 Denis C de Azevedo. All rights reserved.
+//
+
+#import "CoreDataTableViewController.h"
+
+@interface PhotographersCDTVC : CoreDataTableViewController
+
+@property (nonatomic, strong) NSManagedObjectContext *managedObjectContext;
+
+@end
